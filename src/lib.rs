7942a7bf24a6ce766e27e44adb89614800a2a66c
heechan <ryanhe4@gmail.com>
2021-12-20 17:34:14 +0900

    Render Background

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,70 @@
 mod renderer;
 
+use env_logger::Env;
+use log::info;
 use winit::{
   event::{Event, WindowEvent, KeyboardInput, ElementState, VirtualKeyCode},
   event_loop::{ControlFlow, EventLoop},
 };
 
 pub async fn run() {
-  env_logger::init();
+  let env = Env::default()
+    .filter_or("MY_LOG_LEVEL", "pong")
+    .write_style_or("MY_LOG_STYLE", "info");
+
+  env_logger::init_from_env(env);
 
   let event_loop = EventLoop::new();
-  let renderer = pollster::block_on(renderer::Renderer::new(&event_loop));
-  event_loop.run(move |event, _, control_flow| match event {
-    Event::WindowEvent {
-      ref event,
-      window_id,
-    } if window_id == renderer.window.id() => match event {
-      WindowEvent::CloseRequested
-      | WindowEvent::KeyboardInput {
-        input:
-        KeyboardInput {
-          state: ElementState::Pressed,
-          virtual_keycode: Some(VirtualKeyCode::Escape),
-          ..
+  let mut renderer = pollster::block_on(renderer::Renderer::new(&event_loop));
+  event_loop.run(move |event, _, control_flow|
+    match event {
+      Event::WindowEvent {
+        ref event,
+        window_id,
+      } if window_id == renderer.window.id() =>
+        if !renderer.input(event) {
+          //input Event
+          match event {
+            WindowEvent::CloseRequested
+            | WindowEvent::KeyboardInput {
+              input:
+              KeyboardInput {
+                state: ElementState::Pressed,
+                virtual_keycode: Some(VirtualKeyCode::Escape),
+                ..
+              },
+              ..
+            } => *control_flow = ControlFlow::Exit,
+            WindowEvent::Resized(physical_size) => {
+              renderer.resize(*physical_size);
+            }
+            WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+              // new_inner_size is &&mut so we have to dereference it twice
+              renderer.resize(**new_inner_size);
+            }
+            _ => {}
+          }
         },
-        ..
-      } => *control_flow = ControlFlow::Exit,
+      Event::RedrawRequested(_) => {
+        renderer.update();
+        match renderer.render() {
+          Ok(_) => {}
+          // Reconfigure the surface if lost
+          Err(wgpu::SurfaceError::Lost) => renderer.resize(renderer.size),
+          // The system is out of memory, we should probably quit
+          Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+          // All other errors (Outdated, Timeout) should be resolved by the next frame
+          Err(e) => eprintln!("{:?}", e),
+        }
+      }
+      Event::MainEventsCleared => {
+        // RedrawRequested will only trigger once, unless we manually
+        // request it.
+        renderer.window.request_redraw();
+      }
       _ => {}
-    },
-    _ => {}
-  });
+    });
 }
 
-const WIDTH: f32 = 1024.0;
-const HEIGHT: f32 = 720.0;
+const WIDTH: u32 = 1024;
+const HEIGHT: u32 = 720;
--- src/renderer.rs
+++ src/renderer.rs
@@ -5,8 +5,10 @@ use winit::{
 };
 
 use cgmath::*;
+use log::{info};
 use wgpu::SamplerDescriptor;
-use wgpu::util::DeviceExt;
+use winit::dpi::PhysicalSize;
+use winit::event::WindowEvent;
 use crate::{WIDTH, HEIGHT};
 
 pub struct Renderer {
@@ -14,14 +16,18 @@ pub struct Renderer {
   device: wgpu::Device,
   queue: wgpu::Queue,
   config: wgpu::SurfaceConfiguration,
-  size: winit::dpi::PhysicalSize<u32>,
+  pub(crate) size: winit::dpi::PhysicalSize<u32>,
   pub window: Window,
 }
 
 impl Renderer {
   pub async fn new(event_loop: &EventLoop<()>) -> Self {
+    info!("Initialize Renderer::New()");
+
     let window = WindowBuilder::new().build(&event_loop).unwrap();
-    let size = window.inner_size();
+    let size = PhysicalSize { width: WIDTH, height: HEIGHT };
+    // window.set_inner_size(size);
+    window.set_resizable(false);
     let instance = wgpu::Instance::new(wgpu::Backends::all());
     let surface = unsafe { instance.create_surface(&window) };
     let adapter = instance.request_adapter(
@@ -53,5 +59,52 @@ impl Renderer {
       config,
       size,
     }
+  }
+
+  pub fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+    if new_size.width > 0 && new_size.height > 0 {
+      self.size = new_size;
+      self.config.width = new_size.width;
+      self.config.height = new_size.height;
+      self.surface.configure(&self.device, &self.config);
+    }
+  }
+  pub fn input(&mut self, event: &WindowEvent) -> bool {
+    false
+  }
+  pub fn update(&mut self) {
+  }
+  pub fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+    let output = self.surface.get_current_texture()?;
+    let view = output.texture.create_view(&wgpu::TextureViewDescriptor::default());
+    let mut encoder = self.device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+      label: Some("Render encoder"),
+    });
+
+    {
+      let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+        label: Some("Render Pass"),
+        color_attachments: &[wgpu::RenderPassColorAttachment {
+          view: &view,
+          resolve_target: None,
+          ops: wgpu::Operations {
+            load: wgpu::LoadOp::Clear(wgpu::Color {
+              r: 0.1,
+              g: 0.2,
+              b: 0.3,
+              a: 1.0,
+            }),
+            store: true,
+          },
+        }],
+        depth_stencil_attachment: None,
+      });
+    }
+
+    // submit will accept anything that implements IntoIter
+    self.queue.submit(std::iter::once(encoder.finish()));
+    output.present();
+
+    Ok(())
   }
 }
